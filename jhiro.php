082ab780cd783876a27dcc0960df6cae40d4d644
duldulaograce <graceduldulao487@gmail.com>
2025-09-29 18:22:01 +0800

    committed

---
+++ jhiro.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jhiro Zablan – Portfolio</title>
+  <link rel="stylesheet" href="jhiro.css">
+</head>
+<body>
+
+  
+  <header class="hero">
+    <div class="container">
+      <img src="pfp.jpg" alt="Profile Photo" class="profile-pic">
+      <h1>Jhiro Zablan</h1>
+      <p class="tagline">Information Technology Student</p>
+      <a href="contact.php" class="btn">Contact Me</a>
+
+    </div>
+  </header>
+
+  
+  <section id="about" class="section">
+    <h2>About Me</h2>
+    <p>
+       I am a dedicated and detail-oriented Information Technology student with a strong foundation in networking, programming, and cybersecurity. Currently pursuing a degree in IT, I am passionate about leveraging technology to solve real-world problems and continuously expanding my technical skill set. Known for my problem-solving mindset, adaptability, and teamwork abilities, I am eager to apply my academic knowledge in a practical setting through internships or entry-level opportunities. My goal is to contribute to innovative projects while gaining hands-on experience in the tech industry.
+    </p>
+  </section>
+
+
+  <section id="education" class="section bg-light">
+    <h2>Education</h2>
+    <ul>
+      <li><strong>Tarlac State University 
+Bachelor of Science in Information Technology majoy in Web and Mobile Application (2025-2026)</li>
+      <li>Corazon C Aquino High School (ICT) (2018-2019)</li>
+      <li>Corazon C Aquino Senior High School (CCS) (2020-2022)</li>
+    </ul>
+  </section>
+
+  
+  <section id="skills" class="section">
+    <h2>Skills</h2>
+    <div class="skills-grid">
+      <span>Microsoft excel,</span>
+      <span>Hardware Knowledge,</span>
+      <span>Intermediate Hardware Troubleshooting,</span>
+      <span>Data Oriented,</span>
+      <span>Analytic Thinking,</span>
+      <span>Basic Editing,</span>
+    </div>
+  </section>
+
+  <section id="experience" class="section bg-light">
+    <h2>Experience</h2>
+    <p>
+      •  Intern- 
+Corazon C. Aquino HS (March 2022 - Aprill 2022) <br>
+      •  COC LEVEL 1 AND 2 <br>
+  
+
+ <footer>
+    <p>&copy; 2025 WMA-4C</p>
+  </footer>
+
+</body>
+</html>
